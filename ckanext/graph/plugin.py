f4bd288268254532a6f92851795435f495d431ae
Alice Heaton <aliceheaton75@gmail.com>
2014-12-16 13:06:44 +0000

    Cache the result of the graph query for better performance

--- ckanext/graph/plugin.py
+++ ckanext/graph/plugin.py
@@ -4,6 +4,7 @@ import logging
 import datetime
 import dateutil.parser
 from sqlalchemy.exc import DatabaseError, DBAPIError
+from beaker.cache import cache_region
 
 import ckan.plugins as p
 import ckan.plugins.toolkit as toolkit
@@ -104,29 +105,32 @@ class GraphPlugin(p.SingletonPlugin):
 
         (ts_query, where_clause, values) = self._get_request_where_clause(data_dict, field_types)
         # Prepare and run our query
-        query = 'SELECT {select} FROM "{resource_id}" {ts_query} {where_clause} {group_by}'.format(
-            select=select,
-            resource_id=resource_id,
-            where_clause=where_clause,
-            ts_query=ts_query,
-            group_by=group_by
-        )
+        @cache_region('permanent', 'stats_view_query')
+        def run_stats_query(select, resource_id, ts_query, where_clause, group_by):
+            query = 'SELECT {select} FROM "{resource_id}" {ts_query} {where_clause} {group_by}'.format(
+                select=select,
+                resource_id=resource_id,
+                where_clause=where_clause,
+                ts_query=ts_query,
+                group_by=group_by
+            )
 
-        if not is_single_statement(query):
-            raise datastore_db.ValidationError({
-                'query': ['Query is not a single statement.']
-            })
+            if not is_single_statement(query):
+                raise datastore_db.ValidationError({
+                    'query': ['Query is not a single statement.']
+                })
 
-        # The interfaces.IDatastore return SQL to be directly executed
-        # So just use an sqlalchemy connection, rather than the API
-        # So we don't have to faff around converting to pure SQL
-        engine = _get_engine()
-        with engine.begin() as connection:
-            try:
-                query_result = connection.execute(query, values)
-                return query_result.fetchall()
-            except (DatabaseError, DBAPIError):
-                pass
+            # The interfaces.IDatastore return SQL to be directly executed
+            # So just use an sqlalchemy connection, rather than the API
+            # So we don't have to faff around converting to pure SQL
+            engine = _get_engine()
+            with engine.begin() as connection:
+                try:
+                    query_result = connection.execute(query, values)
+                    return query_result.fetchall()
+                except (DatabaseError, DBAPIError):
+                    pass
+        return run_stats_query(select, resource_id, ts_query, where_clause, group_by)
 
 
     def _get_request_where_clause(self, data_dict, field_types):
