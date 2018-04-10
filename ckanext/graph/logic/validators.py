f1636f43ad64952730ad6e0a7d3b554fd3964c80
Alice Butcher <alycejenni@gmail.com>
2018-04-10 13:06:38 +0100

    manual changes to logic

--- ckanext/graph/logic/validators.py
+++ ckanext/graph/logic/validators.py
@@ -1,21 +1,15 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 # encoding: utf-8
 #
 # This file is part of ckanext-graph
 # Created by the Natural History Museum in London, UK
 
-import ckan.plugins as p
-from ckan.common import _
-import ckan.lib.navl.dictization_functions as df
 from sqlalchemy.exc import DataError
 
-Invalid = df.Invalid
-Missing = df.Missing
+from ckan.plugins import toolkit
 
 
 def is_boolean(value, context):
-
     '''Validate a field as a boolean. Assuming missing value means false
 
     :param value: 
@@ -25,14 +19,18 @@ def is_boolean(value, context):
 
     if isinstance(value, bool):
         return value
-    elif (isinstance(value, str) or isinstance(value, unicode)) and value.lower() in [u'true', u'yes', u't', u'y', u'1']:
+    elif (isinstance(value, str) or isinstance(value, unicode)) and value.lower() in [
+        u'true', u'yes', u't', u'y', u'1']:
         return True
-    elif (isinstance(value, str) or isinstance(value, unicode)) and value.lower() in [u'false', u'no', u'f', u'n', u'0']:
+    elif (isinstance(value, str) or isinstance(value, unicode)) and value.lower() in [
+        u'false', u'no', u'f', u'n', u'0']:
         return False
-    elif isinstance(value, Missing):
+    elif isinstance(value, toolkit.missing):
         return False
     else:
-        raise Invalid(_(u'Value must a true/false value (ie. true/yes/t/y/1 or false/no/f/n/0)'))
+        raise toolkit.Invalid(
+            toolkit._(
+                u'Value must a true/false value (ie. true/yes/t/y/1 or false/no/f/n/0)'))
 
 
 def in_list(list_possible_values):
@@ -43,6 +41,7 @@ def in_list(list_possible_values):
         for validated field
 
     '''
+
     def validate(key, data, errors, context):
         '''
 
@@ -53,13 +52,12 @@ def in_list(list_possible_values):
 
         '''
         if not data[key] in list_possible_values:
-            raise Invalid(u'"{0}" is not a valid parameter'.format(data[key]))
+            raise toolkit.Invalid(u'"{0}" is not a valid parameter'.format(data[key]))
 
     return validate
 
 
 def is_date_castable(value, context):
-
     '''Validator to ensure the date is castable to a date field
 
     :param value: param context:
@@ -69,18 +67,19 @@ def is_date_castable(value, context):
 
     if value:
 
-        sql = u'SELECT "{date_field_name}"::timestamp AS date FROM "{resource_id}" LIMIT 1 '.format(
-            date_field_name=value,
-            resource_id=context[u'resource'].id,
-            )
+        sql = u'SELECT "{date_field_name}"::timestamp AS date FROM "{resource_id}" ' \
+              u'LIMIT 1 '.format(date_field_name=value,
+                                 resource_id=context[u'resource'].id, )
 
         data_dict = {
             u'sql': sql
-        }
+            }
 
         try:
-            p.toolkit.get_action(u'datastore_search_sql')({}, data_dict)
+            toolkit.get_action(u'datastore_search_sql')({}, data_dict)
         except DataError:
-            raise Invalid(u'Field {0} cannot be cast into a date. Are you sure it\'s a date field?'.format(value))
+            raise toolkit.Invalid(
+                u'Field {0} cannot be cast into a date. Are you sure it\'s a date '
+                u'field?'.format(value))
 
     return value
