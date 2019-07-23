c79b8e61eb3c54e2d99e768070664fd7ac876a15
Alice Butcher <alycejenni@gmail.com>
2019-07-23 18:28:37 +0100

    Standardise extension documentation/metadata

--- setup.py
+++ setup.py
@@ -4,29 +4,36 @@
 # This file is part of ckanext-graph
 # Created by the Natural History Museum in London, UK
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-version = u'0.1'
+__version__ = u'1.0.0-alpha'
+
+with open(u'README.md', u'r') as f:
+    __long_description__ = f.read()
 
 setup(
-	name=u'ckanext-graph',
-	version=version,
-	description=u'',
-	long_description=u'''\
-	''',
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords=u'',
-	license=u'',
-	packages=find_packages(exclude=[u'ez_setup', u'examples', u'tests']),
-	namespace_packages=[u'ckanext', u'ckanext.graph'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	u'''
+    name=u'ckanext-graph',
+    version=__version__,
+    description=u'A CKAN extension that adds a graph view for resources.',
+    long_description=__long_description__,
+    classifiers=[
+        u'Development Status :: 3 - Alpha',
+        u'Framework :: Flask',
+        u'Programming Language :: Python :: 2.7'
+    ],
+    keywords=u'CKAN data graph',
+    author=u'Natural History Museum',
+    author_email=u'data@nhm.ac.uk',
+    url=u'https://github.com/NaturalHistoryMuseum/ckanext-graph',
+    license=u'GNU GPLv3',
+    packages=find_packages(exclude=[u'tests']),
+    namespace_packages=[u'ckanext', u'ckanext.graph'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points= \
+        u'''
         [ckan.plugins]
-            graph = ckanext.graph.plugin:GraphPlugin
-	''',
-)
+            graph=ckanext.graph.plugin:GraphPlugin
+        ''',
+    )
